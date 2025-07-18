cc924cf75489a88af7245d31db0872058fd8bceb
AncientPatata <110251221+AncientPatata@users.noreply.github.com>
2025-07-18 11:31:13 +0200

    Added DotTrace diagnoser to benchmark SQS

--- benchmark_runner/BenchmoniK/Benchmarks/Queue/SqsThroughputBenchmark.cs
+++ benchmark_runner/BenchmoniK/Benchmarks/Queue/SqsThroughputBenchmark.cs
@@ -5,7 +5,7 @@ using BenchmarkDotNet.Attributes;
 using BenchmoniK.Utils;
 using Microsoft.Extensions.DependencyInjection;
 
-public class SqsThroughputBenchmark: BaseThroughputBenchmark
+public class SqsThroughputBenchmark : BaseThroughputBenchmark
 {
     [GlobalSetup]
     public async Task GlobalSetup()
@@ -18,17 +18,17 @@ public class SqsThroughputBenchmark: BaseThroughputBenchmark
         // Create one client pair for each concurrent runner
         _pullQueueClients.Clear();
         _pushQueueClients.Clear();
-        
+
         for (int i = 0; i < NumConcurrentRunners; i++)
         {
             var serviceProvider = ConfigUtils.BuildServiceProvider(configuration, adapterPath);
 
             var pullQueue = serviceProvider.GetRequiredService<IPullQueueStorage>();
             var pushQueue = serviceProvider.GetRequiredService<IPushQueueStorage>();
-            
+
             await pullQueue.Init(CancellationToken.None);
             await pushQueue.Init(CancellationToken.None);
-            
+
             _pullQueueClients.Add(pullQueue);
             _pushQueueClients.Add(pushQueue);
         }
